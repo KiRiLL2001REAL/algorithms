ba151104d0b43caab06fedefbc15d23a139eaa2e
Kirill Baturin <batyrin2001@yandex.ru>
2020-09-25 22:05:56 +0300

    Some variable names has changed

--- sort/merge/main.cpp
+++ sort/merge/main.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 template<typename T>
-void merge_sort(vector<T>& vec, size_t from, size_t to) {
+void merge_sort(vector<T>& vec, size_t first, size_t last) {
     //O(n log n)
-    if (from != to) { // Если в массиве больше 1 элемента, то его нужно отсортировать
-        size_t middle = (from + to) / 2;
-        merge_sort(vec, from, middle);
-        merge_sort(vec, middle + 1, to);
+    if (first != last) { // Если в массиве больше 1 элемента, то их нужно отсортировать
+        size_t middle = (first + last) / 2;
+        merge_sort(vec, first, middle);
+        merge_sort(vec, middle + 1, last);
 
-        size_t left_i = from;
+        size_t left_i = first;
         size_t right_i = middle + 1;
 
         vector<T> sorted = {}; // Отсортированный массив
-        while ((left_i != middle + 1) && (right_i != to + 1)) { // Обрабатываем оба подмассива, пока какой-то из них не станет пустым
+        while ((left_i != middle + 1) && (right_i != last + 1)) { // Обрабатываем оба подмассива, пока какой-то из них не станет пустым
             if (vec[left_i] < vec[right_i]) {
                 sorted.push_back(vec[left_i]);
                 left_i++;
@@ -30,13 +30,13 @@ void merge_sort(vector<T>& vec, size_t from, size_t to) {
                 sorted.push_back(vec[i]);
             }
         }
-        else if (right_i != to + 1) { // Если правый не пуст, то записываем его
-            for (size_t i = right_i; i < to + 1; i++) {
+        else if (right_i != last + 1) { // Если правый не пуст, то записываем его
+            for (size_t i = right_i; i < last + 1; i++) {
                 sorted.push_back(vec[i]);
             }
         }
-        for (size_t i = from; i < to + 1; i++) {
-            vec[i] = sorted[i - from];
+        for (size_t i = first; i < to + 1; i++) {
+            vec[i] = sorted[i - first];
         }
     }
 }
